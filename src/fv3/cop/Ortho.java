0b8cdbba0bacc7e00e4c40bd8a7499fd9672e471
jdp@ulsf.net <jdp@ulsf.net@1faddcec-d5ef-11de-96bf-fbce4fd84a6e>
2010-06-01 09:10:29 +0000

    upd
    git-svn-id: https://fv3.googlecode.com/svn/trunk@215 1faddcec-d5ef-11de-96bf-fbce4fd84a6e

--- src/fv3/cop/Ortho.java
+++ src/fv3/cop/Ortho.java
@@ -26,8 +26,8 @@ public class Ortho
             this.right = s.maxX;
             this.bottom = s.minY;
             this.top = s.maxY;
-            this.near = s.minZ;
-            this.far = s.maxZ;
+            this.near = s.maxZ;
+            this.far = s.minZ;
         }
         else
             throw new IllegalArgumentException();
@@ -80,13 +80,18 @@ public class Ortho
         }
     }
     public Matrix projection(Camera c){
-        //this.init(c);
+        this.init(c);
 
         Matrix m = c.getProjection();
 
-        double X = ( 2.0 / (right - left));
-        double Y = ( 2.0 / (top - bottom));
-        double Z = (-2.0 / (far - near));
+        double Sx = ( 2.0 / (right - left));
+        double Sy = ( 2.0 / (top - bottom));
+        double Sz = (-2.0 / (far - near));
+
+        m.m00(Sx);
+        m.m11(Sy);
+        m.m22(Sx);
+
         double Tx = ( (right + left) / (right - left));
         if (0.0 != Tx)
             Tx = -(Tx);
@@ -97,9 +102,6 @@ public class Ortho
         if (0.0 != Tz)
             Tz = -(Tz);
 
-        m.m00(X);
-        m.m11(Y);
-        m.m22(X);
         m.m03(Tx);
         m.m13(Ty);
         m.m23(Tz);
