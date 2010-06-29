01cb4777f90969716ef32f005b04082bbd8f0ad8
jdp@ulsf.net <jdp@ulsf.net@1faddcec-d5ef-11de-96bf-fbce4fd84a6e>
2010-06-29 10:17:59 +0000

    buggy vertex data copying
    git-svn-id: https://fv3.googlecode.com/svn/trunk@267 1faddcec-d5ef-11de-96bf-fbce4fd84a6e

--- src/fv3/math/VertexArray.java
+++ src/fv3/math/VertexArray.java
@@ -1,19 +1,20 @@
 /*
  * fv3.math
- * Copyright (c) 2009 John Pritchard, all rights reserved.
- *     
- * This program is free  software: you can redistribute it and/or modify 
- * it under the terms of the GNU Lesser General Public License as published 
- * by the Free Software Foundation, either version 3 of the License, or 
- * (at your option) any later version.
+ * Copyright (C) 2010, John Pritchard, all rights reserved.
  * 
- * This program  is  distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
- * GNU Lesser General Public License for more details.
- * 
- * You should  have received a copy of the GNU Lesser General Public License
- * along with  this program; If not, see <http://www.gnu.org/licenses/>. 
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package fv3.math;
 
@@ -140,6 +141,23 @@ public class VertexArray
             this.type = type;
 
         this.vertices = src.vertices(this.type);
+        this.countVertices = (this.vertices.length / 3);
+
+        this.countFaces = CountFaces(this.type,this.countVertices);
+        if (0 < this.countFaces){
+            this.normals = new double[3 * this.countFaces];
+            this.computeNormals();
+        }
+    }
+    public VertexArray(Type type, double[] vertices){
+        super();
+
+        if (null == type)
+            this.type = Type.TriangleStrip;
+        else
+            this.type = type;
+
+        this.vertices = vertices;
         this.countVertices = (this.vertices.length / 3);
 
         this.countFaces = CountFaces(this.type,this.countVertices);
