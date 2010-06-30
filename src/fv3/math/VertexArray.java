5f3716342f5496a13c3b323d6ce17050dcf19ead
jdp@ulsf.net <jdp@ulsf.net@1faddcec-d5ef-11de-96bf-fbce4fd84a6e>
2010-06-30 02:20:59 +0000

    test
    git-svn-id: https://fv3.googlecode.com/svn/trunk@277 1faddcec-d5ef-11de-96bf-fbce4fd84a6e

--- src/fv3/math/VertexArray.java
+++ src/fv3/math/VertexArray.java
@@ -736,8 +736,8 @@ public class VertexArray
 
                         indeces = FaceIndeces(thatType,thatFaceB);
                         SetVertex(indeces[0], thisFace, 3, thatVertices);
-                        SetVertex(indeces[1], thisFace, 9, thatVertices);
-                        SetVertex(indeces[2], thisFace, 6, thatVertices);
+                        SetVertex(indeces[1], thisFace, 6, thatVertices);
+                        SetVertex(indeces[2], thisFace, 9, thatVertices);
                         break;
                     }
                     case TriangleStrip:
