49be8a21742abb5015b2e91379c96a7d388a65d3
jdp@ulsf.net <jdp@ulsf.net@1faddcec-d5ef-11de-96bf-fbce4fd84a6e>
2010-07-05 10:39:57 +0000

    test
    git-svn-id: https://fv3.googlecode.com/svn/trunk@298 1faddcec-d5ef-11de-96bf-fbce4fd84a6e

--- cylinder/test/Cylinder.java
+++ cylinder/test/Cylinder.java
@@ -47,7 +47,7 @@ public class Cylinder
                         Model model = (Model)cylinder.get(1);
                         Solid s;
 
-                        s = (Solid)model.get(2);
+                        s = (Solid)model.get(3);
                         System.out.println();
                         System.out.println(s.toString("\t"));
                     }
@@ -80,6 +80,7 @@ public class Cylinder
         {
             cylinder.add(new Material(GL2.GL_FRONT_AND_BACK, GL2.GL_AMBIENT_AND_DIFFUSE, Color.Blue));
             cylinder.add(new ShadeModel(GL2.GL_FLAT));
+            cylinder.add(new Enable(GL2.GL_CULL_FACE));
             cylinder.add(new fv3.csg.Cylinder.XY(10,10).compile());
             cylinder.add(new Enable(GL2.GL_NORMALIZE));
         }
