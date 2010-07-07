32186f4808e1ceb33de99e8acf6fac49e7f1fa4b
jdp@ulsf.net <jdp@ulsf.net@1faddcec-d5ef-11de-96bf-fbce4fd84a6e>
2010-07-07 09:11:10 +0000

    test new torus
    git-svn-id: https://fv3.googlecode.com/svn/trunk@318 1faddcec-d5ef-11de-96bf-fbce4fd84a6e

--- torus/test/Torus.java
+++ torus/test/Torus.java
@@ -47,7 +47,7 @@ public class Torus
                         Model model = (Model)torus.get(1);
                         Solid s;
 
-                        s = (Solid)model.get(2);
+                        s = (Solid)model.get(3);
                         System.out.println();
                         System.out.println(s.toString("\t"));
                     }
@@ -74,10 +74,10 @@ public class Torus
 
         Model torus = new Model();
         {
-            torus.add(new Material(GL2.GL_FRONT_AND_BACK, GL2.GL_AMBIENT_AND_DIFFUSE, Color.Blue));
+            torus.add(new Material(GL2.GL_FRONT, GL2.GL_AMBIENT_AND_DIFFUSE, Color.Blue));
             torus.add(new ShadeModel(GL2.GL_FLAT));
-            torus.add(new fv3.csg.Torus.XY(5,10).compile());
             torus.add(new Enable(GL2.GL_NORMALIZE));
+            torus.add(new fv3.csg.Torus.XY(5,10).compile());
         }
         this.add(torus);
 
